7ad59dc4c4613768ddf424a534101c329a771b28
Moises Ariza <arizamoises@gmail.com>
2022-08-04 17:52:01 -0500

    Making API request, add information component and custom sytle with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { ProductCard } from './components/ProductCard/productCard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products').then((result)=>{
+      setProducts(result.data)
+      console.log(products)
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1>Lista de productos</h1>
-      <ProductCard title="Producto de prueba" price="12.000"/>
+      {products && products.map( product => {
+        return(
+          <ProductCard 
+          key={product.id}
+          image={product.image}
+          title={product.title} 
+          price={product.price}/>
+        )
+      })}
+      
     </div>
   )
 }
